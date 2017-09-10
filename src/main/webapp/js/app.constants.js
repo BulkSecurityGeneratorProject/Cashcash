68d67acb5a7ec65232d544fa715926280c0af27e
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2017-09-10 22:15:18 +0200

    Switch to 0.6.2

--- src/main/webapp/js/app.constants.js
+++ src/main/webapp/js/app.constants.js
@@ -2,7 +2,7 @@ import modules from "./modules";
 
 export default modules
     .get('cashcash.appConstant', [])
-    .constant('VERSION', '0.6.1')
+    .constant('VERSION', '0.6.2')
     .constant('DEBUG_INFO_ENABLED', true)
     .name;
 
