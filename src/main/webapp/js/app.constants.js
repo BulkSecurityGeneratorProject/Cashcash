81bf5166a3e095528d663a7357c5484a3842c431
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2017-10-18 21:41:02 +0200

    Update to 0.7.1

--- src/main/webapp/js/app.constants.js
+++ src/main/webapp/js/app.constants.js
@@ -2,7 +2,7 @@ import modules from "./modules";
 
 export default modules
     .get('cashcash.appConstant', [])
-    .constant('VERSION', '0.7.0')
+    .constant('VERSION', '0.7.1')
     .constant('DEBUG_INFO_ENABLED', true)
     .name;
 
