c9bc3d860827f0c46e2e3834df1f8e4cafc824aa
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2017-10-19 22:22:19 +0200

    0.7.2

--- src/main/webapp/js/app.constants.js
+++ src/main/webapp/js/app.constants.js
@@ -2,7 +2,7 @@ import modules from "./modules";
 
 export default modules
     .get('cashcash.appConstant', [])
-    .constant('VERSION', '0.7.1')
+    .constant('VERSION', '0.7.2')
     .constant('DEBUG_INFO_ENABLED', true)
     .name;
 
