80bd3695e042bbcce9179f23d77cc912380d5285
Samuel Gagnepain <samuel.gagnepain@gmail.com>
2017-12-03 20:07:19 +0100

    Replace the footer with a vue component

--- src/main/webapp/js/components/components.module.js
+++ src/main/webapp/js/components/components.module.js
@@ -2,12 +2,12 @@ import modules from "../modules";
 import util from "./util/util.module";
 import login from "./login/login.module";
 import form from "./form/form.module";
-import footer from "./footer/footer.component";
 import cashTransactionEdit from "./cash-transaction/cash-transaction-edit-panel.component";
 import accountInteractionPanel from "./account-interaction-panel/account-interaction-panel.components";
 import alert from "./alert/alert.module";
 import cashTransactionEditVue from "./cash-transaction/cash-transaction-edit-panel.vue";
 import cashTransactionEditListVue from "./cash-transaction/cash-transaction-edit-list.vue";
+import footerVue from "./footer/footer.vue";
 
 export default modules
     .get('cashcash.components', [
@@ -16,10 +16,10 @@ export default modules
         form,
         alert
     ])
-    .component('footer', footer)
     .component('cashTransactionEdit', cashTransactionEdit)
     .component('accountInteractionPanel', accountInteractionPanel)
     .value('CashTransactionEditVue', cashTransactionEditVue)
     .value('CashTransactionEditListVue', cashTransactionEditListVue)
+    .value('FooterVue', footerVue)
     .name;
 
---
+++ src/main/webapp/js/components/footer/footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="footer">
+        <div class="col-lg-12">
+            <p class="text-center">Copyright © Samuel GAGNEPAIN {{currentYear}}</p>
+            <p class="text-center"><span>v{{version}}</span></p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterVue',
+        props: {
+            version: String
+        },
+        data() {
+            return {
+                currentYear: new Date().getFullYear()
+            }
+        }
+    }
+</script>
